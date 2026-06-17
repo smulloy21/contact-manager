8aa08f9a6064ab6e14c3b9fcdc9801ef6b8c3777
Shannon Mulloy <smulloy21@gmail.com>
2026-06-16 21:03:39 -0400

    fix svg xmlns

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -267,14 +267,14 @@ class ContactsApp {
     editBtn.classList.add('my-3', 'mx-1', 'btn', 'btn-primary');
     editBtn.dataset.action = 'edit';
     editBtn.setAttribute('aria-label', 'Edit contact');
-    editBtn.innerHTML = `<svg xmlns="http://w3.org" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>`;
+    editBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>`;
 
     let deleteBtn = document.createElement('button');
     deleteBtn.type = 'button';
     deleteBtn.classList.add('my-3', 'mx-1', 'btn', 'btn-danger');
     deleteBtn.dataset.action = 'delete';
     deleteBtn.setAttribute('aria-label', 'Delete contact');
-    deleteBtn.innerHTML = `<svg xmlns="http://w3.org" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /></svg>`;
+    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /></svg>`;
 
     wrapper.append(editBtn, deleteBtn);
     return wrapper;
